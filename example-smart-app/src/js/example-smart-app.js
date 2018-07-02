1b8b747ecff1eae2b793fab456441a3e6c846614
Gayathri Nagaraj <gayu.b2w@gmail.com>
2018-07-02 15:41:25 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -38,7 +38,7 @@
        var base64Url1 = access_token.split('.')[1];
        var base641 = base64Url1.replace('-', '+').replace('_', '/');
        console.log(JSON.parse(window.atob(base641)));
-       var temp ="urn:cerner:authorization:claims:version:1";
+       var temp ="urn.cerner.authorization.claims.versio.1";
        persona=(JSON.parse(window.atob(base641))).temp;
        console.log(persona);
       
