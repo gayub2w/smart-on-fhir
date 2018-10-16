0c70455eeaba247a1d6e034551088e422f115be0
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2018-10-16 16:55:37 -0700

    Update global.js

--- example-smart-app/src/js/global.js
+++ example-smart-app/src/js/global.js
@@ -4,3 +4,7 @@ var pat_fname="";
 var pat_lname="";
 var gender2="";
 var dobstr2="";
+
+var Series1 = [{name: 'PROMIS Bank v1.0 - Pain Interference',data: [0,15, 10, 30, 30, 0, 0, 8, 56.6]}, {name: 'PROMIS Bank v2.0 - Physical Function',data: [0,20, 15, 8, 11, 5, 33, 0, 48.3]}];
+
+export { Series1 };
