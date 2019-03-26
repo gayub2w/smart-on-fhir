e2bd3351ad924d7c7da04c79efa3b196b7c75528
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2019-03-26 14:51:02 -0700

    Update global.js

--- example-smart-app/src/js/global.js
+++ example-smart-app/src/js/global.js
@@ -845,7 +845,7 @@ function displayList(){
 	var settings3 = {
 			"async": false,
 			"crossDomain": true,
-			"url": baseurl+"ProcedureRequest?subject=http://hl7.org/fhir/sid/us-ssn/Patient/"+patID+"&_count=20&intent=order&status=active",
+			"url": baseurl+"ProcedureRequest?subject=http://hl7.org/fhir/sid/us-ssn/Patient/"+patID+"&_count=200&intent=order&status=active",
 		
 			"method": "GET"
 	}
